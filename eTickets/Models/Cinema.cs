39caa06aecad1fbbe1cc7fbe39dd4a758912e74b
Ervis Trupja <ervistrupja@outlook.com>
2021-05-21 15:36:18 +0200

    Added cinema model

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
