8bfe79b142e566a79ab8ed6d387c5fce313ae123
Ervis Trupja <ervistrupja@outlook.com>
2021-05-21 17:38:25 +0200

    Added AppDbInitializer

--- eTickets/Data/Enums/MovieCategory.cs
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -10,6 +10,8 @@ namespace eTickets.Data
         Action = 1,
         Comedy,
         Drama,
-        Documentary
+        Documentary,
+        Cartoon,
+        Horror
     }
 }
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -58,6 +58,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
