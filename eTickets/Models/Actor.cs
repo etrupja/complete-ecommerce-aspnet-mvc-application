348d6c9c52526dc9174b420ec46a9f6535ac67d0
Ervis Trupja <ervistrupja@outlook.com>
2021-05-21 15:25:50 +0200

    Added actor model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
