ea1f178b7af9605fad48669930aadfa4c2e9fefe
Ervis Trupja <ervistrupja@outlook.com>
2021-07-19 18:05:13 +0200

    added actors controller

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
