8c086d04be5e0d4181e01ad6d2c74c684a0f8d27
Ervis Trupja <ervistrupja@outlook.com>
2021-08-04 14:46:41 +0200

    added cinemas list

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -19,8 +19,8 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allProducers = await _context.Cinemas.ToListAsync();
-            return View();
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View(allCinemas);
         }
     }
 }
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -11,8 +11,12 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Cinema Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         //Relationships
