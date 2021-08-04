4d3a5ef4362265f73b0589a8e791387b8b86d95f
Ervis Trupja <ervistrupja@outlook.com>
2021-08-04 14:25:53 +0200

    added producers list

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -20,7 +20,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allProducers = await _context.Producers.ToListAsync();
-            return View();
+            return View(allProducers);
         }
     }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -11,7 +11,7 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Profile Picture URL")]
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
