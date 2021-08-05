4bfbd935a73b6a681c7451ce512aea674f91480d
Ervis Trupja <ervistrupja@outlook.com>
2021-08-05 13:00:34 +0200

    addscoped configuration added

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,9 @@ namespace eTickets
 
             //DbContext configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
+            //Services configuration
+            services.AddScoped<IActorsService, ActorsService>();
 
             services.AddControllersWithViews();
         }
