ce5a2622d80b82d2e39c1457bd0628319faa34b3
Ervis Trupja <ervistrupja@outlook.com>
2021-08-05 11:26:12 +0200

    added Iactorsservice

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
