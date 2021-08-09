e849e11277dcf961221ad615d8023824963aacc9
Ervis Trupja <ervistrupja@outlook.com>
2021-08-09 14:00:03 +0200

    added update and deleteasync methods

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,17 +17,20 @@ namespace eTickets.Data.Base
 
         public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task UpdateAsync(int id, T entity)
         {
-            throw new NotImplementedException();
+            EntityEntry entityEntry =  _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
         }
     }
 }
--- eTickets/Data/Base/IEntityBaseRepository.cs
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -10,7 +10,7 @@ namespace eTickets.Data.Base
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task AddAsync(T entity);
-        Task<T> UpdateAsync(int id, T entity);
+        Task UpdateAsync(int id, T entity);
         Task DeleteAsync(int id);
     }
 }
