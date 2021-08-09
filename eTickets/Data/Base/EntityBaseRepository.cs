4e36ef7af4a9d41511f349fb9fd65b79898a2a89
Ervis Trupja <ervistrupja@outlook.com>
2021-08-09 13:49:48 +0200

    added addasync method

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -14,27 +14,15 @@ namespace eTickets.Data.Base
             _context = context;
         }
 
-        public Task AddAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            var result = await _context.Set<T>().ToListAsync();
-            return result;
-        }
-
-        public async Task<T> GetByIdAsync(int id)
-        {
-            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
-            return result;
-        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
         public Task<T> UpdateAsync(int id, T entity)
         {
