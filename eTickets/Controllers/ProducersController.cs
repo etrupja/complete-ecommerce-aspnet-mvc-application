c2d5f7efc43147a0b7f2e4a86c9569a8fe6765fd
Ervis Trupja <ervistrupja@outlook.com>
2021-08-10 14:52:43 +0200

    delete producer

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -68,5 +68,23 @@ namespace eTickets.Controllers
             }
             return View(producer);
         }
+
+        //GET: producers/delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
