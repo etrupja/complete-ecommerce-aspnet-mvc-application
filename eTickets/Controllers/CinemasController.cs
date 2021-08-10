7db086bde87889c438b19cb51cdf961e53a87628
Ervis Trupja <ervistrupja@outlook.com>
2021-08-10 15:25:23 +0200

    adding new cinema to the database

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -22,6 +23,21 @@ namespace eTickets.Controllers
         {
             var allCinemas = await _service.GetAllAsync();
             return View(allCinemas);
+        }
+
+
+        //Get: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo,Name,Description")]Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -13,11 +13,15 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
+
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema name is required")]
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         //Relationships
