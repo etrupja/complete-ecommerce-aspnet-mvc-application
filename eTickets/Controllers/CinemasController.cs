3d3a48c311e1c14d08172f9743919a6cec7fe70e
Ervis Trupja <ervistrupja@outlook.com>
2021-08-10 16:13:44 +0200

    deleted cinema

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -63,5 +63,23 @@ namespace eTickets.Controllers
             await _service.UpdateAsync(id, cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinemas/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
