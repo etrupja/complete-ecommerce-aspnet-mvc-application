13cc0e74285c5c6d993fbf7d4ba8507bb1df37dd
Ervis Trupja <ervistrupja@outlook.com>
2021-08-14 14:04:40 +0200

    added generic method to get all movies including cinema

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -20,7 +20,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _service.GetAllAsync();
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
             return View(allMovies);
         }
     }
--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
@@ -31,6 +32,15 @@ namespace eTickets.Data.Base
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return await query.ToListAsync();
+
+        }
+
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
         public async Task UpdateAsync(int id, T entity)
