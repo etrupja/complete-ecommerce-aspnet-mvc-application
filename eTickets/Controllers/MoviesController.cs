5175133853cd039fa742dd5af7ae608c6c10cbdf
Ervis Trupja <ervistrupja@outlook.com>
2021-09-26 16:01:39 +0200

    added search

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -26,6 +26,19 @@ namespace eTickets.Controllers
             return View(allMovies);
         }
 
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", allMovies);
+        }
+
         //GET: Movies/Details/1
         public async Task<IActionResult> Details(int id)
         {
