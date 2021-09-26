e57ed4e3ace53f7a87e292901df3ce5f718aba75
Ervis Trupja <ervistrupja@outlook.com>
2021-09-26 18:49:41 +0200

    remove item from shopping cart

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -34,13 +34,24 @@ namespace eTickets.Controllers
             return View(response);
         }
 
-        public async Task<RedirectToActionResult> AddItemToShoppingCart(int id)
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
         {
             var item = await _moviesService.GetMovieByIdAsync(id);
 
             if (item != null)
             {
                 _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
             }
             return RedirectToAction(nameof(ShoppingCart));
         }
