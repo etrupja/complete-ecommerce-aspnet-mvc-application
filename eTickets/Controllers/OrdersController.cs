6cc484d7a2c47596f34adcfa05d3d83fe1350cc7
Ervis Trupja <ervistrupja@outlook.com>
2021-09-30 16:02:58 +0200

    added orders list

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -22,6 +22,14 @@ namespace eTickets.Controllers
             _ordersService = ordersService;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
