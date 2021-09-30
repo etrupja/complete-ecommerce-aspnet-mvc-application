1d81f20399422ddcf07821f3720c0056ae4ea56a
Ervis Trupja <ervistrupja@outlook.com>
2021-09-30 14:47:52 +0200

    added and configured ordersservice

---
+++ eTickets/Data/Services/OrdersService.cs
@@ -0,0 +1,48 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        {
+            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Movie).Where(n => n.UserId == userId).ToListAsync();
+            return orders;
+        }
+
+        public async Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            var order = new Order()
+            {
+                UserId = userId,
+                Email = userEmailAddress
+            };
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            foreach (var item in items)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Amount = item.Amount,
+                    MovieId = item.Movie.Id,
+                    OrderId = order.Id,
+                    Price = item.Movie.Price
+                };
+                await _context.OrderItems.AddAsync(orderItem);
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -37,6 +37,7 @@ namespace eTickets
             services.AddScoped<IProducersService, ProducersService>();
             services.AddScoped<ICinemasService, CinemasService>();
             services.AddScoped<IMoviesService, MoviesService>();
+            services.AddScoped<IOrdersService, OrdersService>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
