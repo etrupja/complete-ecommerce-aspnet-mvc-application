886bfd647239dc2d6696edf76275131966088519
Ervis Trupja <ervistrupja@outlook.com>
2021-09-30 14:28:06 +0200

    added shoppingcartsummary viewcomponent

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
