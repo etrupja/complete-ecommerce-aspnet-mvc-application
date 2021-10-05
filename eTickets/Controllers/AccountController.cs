5244ff607af7cd79b42bb792ba16ff4eaa6e71cc
Ervis Trupja <ervistrupja@outlook.com>
2021-10-05 15:15:36 +0200

    complete registration process

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Static;
 using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
@@ -52,6 +53,32 @@ namespace eTickets.Controllers
 
 
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
 
     }
 }
