ab2f0d4fbee4f3cbdf2c2b41d44bc7a20abdd3ef
Ervis Trupja <ervistrupja@outlook.com>
2021-10-24 13:00:37 +0200

    updated default routing

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -86,7 +86,7 @@ namespace eTickets
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
 
             //Seed database
